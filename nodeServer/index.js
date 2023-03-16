f5352663e7ef5cabadc97c3ee872db22125385b7
L0RD-ZER0 <68327382+L0RD-ZER0@users.noreply.github.com>
2023-03-16 16:39:27 +0530

    Integrate ``socket.io`` with ``experss.js`` app to serve the ``browserClient`` package.

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,24 +1,35 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const io=require('socket.io')(8000)        
-        
-        const users = {};
 
-io.on('connection', socket =>{
+const users = {};
+
+io.on('connection', socket => {
     //if any new user joins, let other users connected to the server know
-    socket.on('new-user-joined', name =>{
-        console.log("New user",name);
+    socket.on('new-user-joined', name => {
+        console.log("New user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     //if someone sends a message,broadcast it to other people
-    socket.on('send',message =>{
-        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     });
 
     //if someone leaves the chat, let others know
-    socket.on('disconnect', message =>{
+    socket.on('disconnect', message => {
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 })
+
+app.use('/', express.static(path.join(path.dirname(__dirname), "browserClient")));
+server.listen(8000, () => {
+    console.log('listening on *:8000');
+});
